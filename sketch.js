789c61e5d4b2bf6bc0a990a20046ccd146791ee6
deboracitelli <debora.bassai@escola.pr.gov.br>
2024-08-14 14:52:53 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+//variaveis da bolinha
+let xBolinha = 100;
+let yBolinha = 200; 
+let diametro = 20;
+let raio = diametro / 2;
+
+// variaveis do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variaveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//placar do jogo
+let meusPontos = 0;
+let pontosDoOponente = 0;
+
+
+//sons do jogo
+let raquetada;
+let ponto;
+let trilha;
+
+let colidiu = false;
+
+function setup() {
+  createCanvas(600, 400);
+  trilha.loop();
+  }
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete(xRaquete, yRaquete);
+  movimentaMinhaRaquete();
+  verificaColisaoRaquete(xRaquete, yRaquete);
+  verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);
+  mostraRaquete(xRaqueteOponente, yRaqueteOponente);
+  movimentaRaqueteOponente();
+  incluiPlacar()
+  marcaPonto();
+  }
+function mostraBolinha() {
+  circle(xBolinha, yBolinha, diametro);
+  }
+
+function movimentaBolinha() {
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+  }
+
+function verificaColisaoBorda() {
+  if (xBolinha + raio > width || xBolinha - raio < 0) {
+    velocidadeXBolinha *= -1;
+    }
+  if (yBolinha + raio > height || yBolinha - raio < 0) {
+    velocidadeYBolinha *= -1;
+    }
+  }
+
+function mostraRaquete(x,y) {
+  rect(x, y, raqueteComprimento, raqueteAltura);
+    
